a7149fcd4c827cc4af6a3476b0cb7fce20b80f3e
Vat2002 <vathila20@outlook.com>
2022-04-18 15:20:47 +0530

    Implemented the searchMoviesWithActor function

--- app/src/main/java/com/example/mobilecoursework2/SearchForActors.kt
+++ app/src/main/java/com/example/mobilecoursework2/SearchForActors.kt
@@ -1,21 +1,23 @@
 package com.example.mobilecoursework2
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.example.mobilecoursework2.entities.Actor
-import com.example.mobilecoursework2.entities.Movie
+import androidx.room.Room
+import com.example.mobilecoursework2.entities.relations.ActorsWithMovies
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
-import org.w3c.dom.Text
+import kotlinx.coroutines.withContext
 
 class SearchForActors : AppCompatActivity(){
 
     private lateinit var moviesDao: MovieDao
-
+    private lateinit var listOfMovies : List<ActorsWithMovies>
     lateinit var actorInputData : String
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,24 +26,46 @@ class SearchForActors : AppCompatActivity(){
 
         val searchActorBT = findViewById<Button>(R.id.searchActorInActBt)
         val getSearchActor = findViewById<TextView>(R.id.searchActorPT)
+        val db = Room.databaseBuilder(this, MoviesDatabase::class.java, "Movies_Database").build()
+        moviesDao = db.movieDao()
 
         searchActorBT.setOnClickListener {
             actorInputData = getSearchActor.text.toString()
-            searchActor()
+            Log.d("Input",actorInputData)
+            searchMoviesWithActor(actorInputData)
         }
     }
 
-    private fun searchActor(){
+    @SuppressLint("SetTextI18n")
+    private fun searchMoviesWithActor(query : String){
 
         val viewSearchedActor = findViewById<TextView>(R.id.viewSearchedActors)
         viewSearchedActor.movementMethod = ScrollingMovementMethod()
 
         runBlocking {
             launch {
-                /*if (actorInputData == moviesDao.getAllActor().toString()){
-                    viewSearchedActor.text = actorInputData
-                }*/
-                viewSearchedActor.text = moviesDao.getActor().toString()
+                withContext(Dispatchers.IO){
+                    listOfMovies = moviesDao.getActorsWithMovies(query)
+                    val sizeList = listOfMovies.size
+                    Log.d("List Size", "$sizeList")
+                }
+                for(film in listOfMovies){
+                    Log.d("Insert","$film")
+                }
+                if(listOfMovies.size > 1){
+                    for(loop in listOfMovies){
+                        for(movie in loop.movies){
+                            val movieTitle = movie.movieTitle
+                            val actorName = loop.actors.actorName
+                            Log.d("Actor dets","$movieTitle + $actorName")
+
+                            viewSearchedActor.text = "Actor Name:$actorName\nMovie Title:$movieTitle"
+                        }
+                    }
+                } else if(listOfMovies.size == 1){
+                    val movieTitle = listOfMovies[0].movies[0].movieTitle
+                    viewSearchedActor.text =  "Movie Title:$movieTitle"
+                }
             }
         }
     }
